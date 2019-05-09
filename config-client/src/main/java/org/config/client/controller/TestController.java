33647739a16811d4b65a118817e567db1a8d0abb
168 <191720653@qq.com>
2019-05-09 22:41:20 +0800

    config

---
+++ config-client/src/main/java/org/config/client/controller/TestController.java
@@ -0,0 +1,18 @@
+package org.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${test}")
+    private String test;
+
+    @RequestMapping("/test")
+    public String test() {
+        return test;
+    }
+
+}
